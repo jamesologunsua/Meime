0c5235e6e0a006122ba7330cda6b33dd80004564
James Ologunsua <jamesologunsua@gmail.com>
2026-02-25 14:37:47 +0100

    Second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+/* import { BrowserRouter } from 'react-router-dom' */
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
-
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
